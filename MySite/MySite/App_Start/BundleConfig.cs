5c4bb1ea92d9d137c79e2cc8fda093e9b557ed90
ndewany <ndewany@gmail.com>
2016-03-28 18:57:14 +0400

    my first comment

---
+++ MySite/MySite/App_Start/BundleConfig.cs
@@ -0,0 +1,39 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MySite
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            // Used for easyTree
+            bundles.Add(new ScriptBundle("~/bundles/easyTreeScript").Include(
+                      "~/Scripts/easyTree/easyTree.js"));
+
+            // Used for easyTree
+            bundles.Add(new StyleBundle("~/Content/easyTreeCss").Include(
+                      "~/Content/easyTree/easyTree.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ MySite/MySite/Models/IdentityModels.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MySite.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+    public class ApplicationUserAdmin : IdentityUser
+    {
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("MySiteUsersConnectionString")
+        {
+        }
+    }
+
+    public class ApplicationDbContextAdmin : IdentityDbContext<ApplicationUserAdmin>
+    {
+        public ApplicationDbContextAdmin()
+            : base("MySiteAdminUsersConnectionString")
+        {
+        }
+    }
+}
